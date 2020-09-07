2280b60fdda439dcce5760be9ee504e13719ad3c
fxrcode <fxrcode@gmail.com>
2020-09-07 01:00:55 -0700

    extra-curricular: lesson3 SQL Aggregation - HAVING

--- Extra-Curricular/SQL/L3-Aggregation.sql
+++ Extra-Curricular/SQL/L3-Aggregation.sql
@@ -2,6 +2,10 @@
 Lesson 3 - SQL Aggregation
 */
 
+/*****
+    --- GROUP
+******/
+
 /*
 Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.
 */
@@ -70,3 +74,79 @@ JOIN sales_reps s
 ON r.id = s.region_id
 GROUP BY r.name
 ORDER BY num_reps;
+
+
+/*****
+    --- HAVING
+******/
+
+-- How many of the sales reps have more than 5 accounts that they manage?
+SELECT s.id, s.name, COUNT(*) num_accounts
+FROM accounts a
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.id, s.name
+HAVING COUNT(*) > 5
+ORDER BY num_accounts desc;
+
+-- How many accounts have more than 20 orders?
+select a.id, COUNT(o.id)
+from accounts a
+join orders o
+on a.id = o.account_id
+group by a.id
+HAVING COUNT(o.id) > 20
+order by COUNT(o.id) desc
+
+-- Which account has the most orders?
+SELECT a.id, a.name, COUNT(*) num_orders
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+ORDER BY num_orders DESC
+LIMIT 1;
+
+-- Which accounts spent more than 30,000 usd total across all orders?
+select a.id, SUM(o.total_amt_usd)
+from accounts a
+join orders o
+on a.id = o.account_id
+group by a.id
+HAVING SUM(o.total_amt_usd) > 30000
+order by SUM(o.total_amt_usd) desc
+
+-- Which accounts spent less than 1,000 usd total across all orders?
+select a.id, SUM(o.total_amt_usd)
+from accounts a
+join orders o
+on a.id = o.account_id
+group by a.id
+HAVING SUM(o.total_amt_usd) <1000
+order by SUM(o.total_amt_usd) desc
+
+-- Which account has spent the most with us?
+
+
+-- Which account has spent the least with us?
+
+
+-- Which accounts used facebook as a channel to contact customers more than 6 times?
+-- Which account used facebook most as a channel?
+select a.id, COUNT(w.id)
+from accounts a
+join web_events w
+on a.id = w.account_id
+GROUP by a.id
+HAVING COUNT(w.id) > 6 and w.channel = 'facebook'
+order by COUNT(w.id) desc
+
+
+-- Which channel was most frequently used by most accounts?
+SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+GROUP BY a.id, a.name, w.channel
+ORDER BY use_of_channel DESC
+LIMIT 10;
